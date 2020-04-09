e5646dab8d828684b9584fe191189534e11a3726
EduhDev <eduardocarvalho970@gmail.com>
2020-04-09 00:03:55 -0300

    Implementa alteracao de tema

--- src/components/PostItem/styles.js
+++ src/components/PostItem/styles.js
@@ -2,18 +2,18 @@ import styled from "styled-components"
 import { Link } from "gatsby"
 
 export const PostItemLink = styled(Link)`
-  color: #8899a6;
+  color: var(--texts);
   display: flex;
   text-decoration: none;
 
   &:hover {
-    color: #1fa1f2;
+    color: var(--highlight);
   }
 `
 
 export const PostItemWrapper = styled.section`
   align-items: center;
-  border-bottom: 1px solid #38444d;
+  border-bottom: 1px solid var(--borders);
   display: flex;
   padding: 2rem 3rem;
   width: 100%;
@@ -23,7 +23,7 @@ export const PostItemTag = styled.div`
   align-items: center;
   background: ${props => (props.background ? props.background : "#41aa12")};
   border-radius: 50%;
-  color: #fff;
+  color: var(--white);
   display: flex;
   font-size: 1.3rem;
   font-weight: 700;
